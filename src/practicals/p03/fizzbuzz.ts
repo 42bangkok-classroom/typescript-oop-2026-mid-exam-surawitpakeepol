c6359172c4a6392dad79da4c19cf8f9c0dfdf079
surawitpakeepol <surawitpakeepol@gmail.com>
2026-01-21 14:42:39 +0700

    mid_oop

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,19 @@
-export{}
+const n: number = parseFloat(process.argv[2]);
+if(isNaN(n)|| n < 1 ||!Number.isInteger(n)){
+    process.exit();
+}
+else{
+    for(let i = 1;i<=n;i++){
+        
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0){
+            console.log('Fizz');
+        }else if(i % 5 === 0){
+            console.log('Buzz');
+        }
+        else{
+            console.log(`${i}`);
+        }
+        }
+    }
