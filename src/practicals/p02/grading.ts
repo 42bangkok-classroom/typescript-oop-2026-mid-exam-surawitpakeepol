deccb585d3ad2e866221abf10ebea348276bab0a
surawitpakeepol <surawitpakeepol@gmail.com>
2026-01-21 14:43:22 +0700

    mid_oop

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,24 @@
+const input = process.argv[2];
+const point = Number(input);
+
+if (Number.isNaN(point) || input === "" ||point < 0 || point > 100) {
+    console.log("Invalid Input");
+}
+else {
+    if (point >= 80){
+        console.log("Grade is A");
+    }
+    else if (point >= 70){
+        console.log("Grade is B");
+    }
+    else if (point >= 60){
+        console.log("Grade is C");
+    }
+    else if (point >= 50){
+        console.log("Grade is D");
+    }
+    else{
+        console.log("Grade is F");
+    }
+}
 export{}
